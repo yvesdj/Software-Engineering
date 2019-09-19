1a8207d8d6592565360063fef6768c51185081f9
yvesdj <yves.dejonghe@student.ap.be>
2019-09-19 09:06:18 +0200

    Added method AddNumbers to Yves.cs

--- Labo2/Labo2/Yves.cs
+++ Labo2/Labo2/Yves.cs
@@ -8,6 +8,14 @@ namespace Labo2
 {
     class Yves
     {
-     
+        public int Getal1 { get; set; }
+        public int Getal2 { get; set; }
+
+        public int AddNumbers()
+        {
+            int som = Getal1 + Getal2;
+
+            return som;
+        }
     }
 }
