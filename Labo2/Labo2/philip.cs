38c081b5b51f555de9431f7be185a6842a0d4c64
Philip De Rudder <philip_de_rudder@hotmail.com>
2019-09-23 16:24:47 +0200

    oioioi

---
+++ Labo2/Labo2/philip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo2
+{
+    class philip
+    {
+
+        public void hallo()
+        {
+            Console.WriteLine("oi");
+        }
+
+    }
+}
